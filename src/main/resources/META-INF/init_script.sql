fa18739bd84ae7e97726e11dfa1a4684711e2c7d
nuinattapon <nattapon.subanake@gmail.com>
2021-08-20 20:07:57 +0700

    First commit

---
+++ src/main/resources/META-INF/init_script.sql
@@ -0,0 +1,25 @@
+INSERT INTO pokemontype VALUES (1, 'Normal');
+INSERT INTO pokemontype VALUES (2, 'Fighting');
+INSERT INTO pokemontype VALUES (3, 'Flying');
+INSERT INTO pokemontype VALUES (4, 'Poison');
+INSERT INTO pokemontype VALUES (5, 'Ground');
+INSERT INTO pokemontype VALUES (6, 'Rock');
+INSERT INTO pokemontype VALUES (7, 'Bug');
+INSERT INTO pokemontype VALUES (8, 'Ghost');
+INSERT INTO pokemontype VALUES (9, 'Steel');
+INSERT INTO pokemontype VALUES (10, 'Fire');
+INSERT INTO pokemontype VALUES (11, 'Water');
+INSERT INTO pokemontype VALUES (12, 'Grass');
+INSERT INTO pokemontype VALUES (13, 'Electric');
+INSERT INTO pokemontype VALUES (14, 'Psychic');
+INSERT INTO pokemontype VALUES (15, 'Ice');
+INSERT INTO pokemontype VALUES (16, 'Dragon');
+INSERT INTO pokemontype VALUES (17, 'Dark');
+INSERT INTO pokemontype VALUES (18, 'Fairy');
+
+INSERT INTO pokemon VALUES (1, 'Bulbasaur', 12);
+INSERT INTO pokemon VALUES (2, 'Charmander', 10);
+INSERT INTO pokemon VALUES (3, 'Squirtle', 11);
+INSERT INTO pokemon VALUES (4, 'Caterpie', 7);
+INSERT INTO pokemon VALUES (5, 'Weedle', 7);
+INSERT INTO pokemon VALUES (6, 'Pidgey', 3);
